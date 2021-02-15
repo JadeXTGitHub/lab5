24fc1e4cff6566fc48c591be7c617fbb707e2166
JadeXTGitHub <78270947+JadeXTGitHub@users.noreply.github.com>
2021-02-15 08:22:24 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jennifer Thomas - STIM 3320 - Session 5 Lab</title>
+</head>
+
+<style>
+body {
+  font-family: arial, sans-serif;
+  background-color: rgb(22, 73, 60);
+  background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQMAAADCCAMAAAB6zFdcAAAAA1BMVEXd3d3u346CAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIC3AcUIAAFkqh/QAAAAAElFTkSuQmCC);
+  color:rgb(12, 12, 12)  }
+h1 {
+  font-family: 'Times New Roman', Times, serif, sans-serif;
+  background-color: rgb(114, 90, 134);
+  color:white;
+  text-align: center;
+  font-weight: bold;}
+h2 {
+  font-family: 'Courier New', Courier, monospace, sans-serif;
+  background-color: rgb(3, 2, 49);
+  color: white}
+h3 {
+	font-family: 'Times New Roman', Times, serif, sans-serif;
+	background-color: rgb(84, 12, 218);
+	color: white}
+
+a {
+    color: #0a0a0a;}
+
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 20%;}
+
+.new {
+  font-style: italic;
+  font-weight: bold  }
+
+.trial {
+  background-color: black;
+  color:blue  }
+
+.email {
+  color: rgb(38, 126, 75)   }
+
+.dbu {
+  color:blue  }
+
+
+
+</style>
+
+</head>
+
+   <body>
+	 
+	
+	<h1>Jennifer Thomas</h1> 		
+		  <hr/>
+	  
+	<h2>About Me</h2>
+	  <p><b><i>Hi y&#39;all, my name is Jennifer Thomas, and I am studying Web Page Design at <a href="https://dbu.edu">Dallas Baptist University</a>, primarily to satisfy requirements for my MIS BS degree, and secondarily because I enjoy technology, learning, and deepening skills in all areas. </i></b></p>
+	  <img src= "https://thumbs.dreamstime.com/b/clear-vision-4581472.jpg" class="center" alt="glasses" >
+
+		
+	  <p class="dbu"><a href="https://dbu.edu"> <img src="https://www.dbu.edu/templates/uber/images/dbu-logo.png" alt="DBU" class="center"> </a> </p>
+	  
+	<h2>My Favorite Quote</h2>
+	  <blockquote>&quot;Tell me and I forget. Teach me and I remember. Involve me and I learn.&quot; –Benjamin Franklin </blockquote>
+	  
+	<h3> Things I enjoy</h3>
+	<ol>
+	  <li>technology</li>
+	  <li>working out</li>
+	  <li>spending time with my dogs</li>
+	  <li>learning</li>
+	</ol>
+	  
+	<h3>Adjectives that describe me</h3>
+	<ul>
+	<li>Inquisitive</li>
+	<li>Motivated</li>
+	<li>Dependable</li>
+	<li>Empathetic</li>
+	<li>Open-minded</li>
+	</ul>
+	  
+	<h3> My Favorite Web Sites</h3>
+	<a href="https://lab3thomasj.herokuapp.com/"> A copy of this page running on the internet</a> - <mark>YAY I was able to publish this page on the internet using the build pipeline and the Heroku hosting platform</mark><br>
+	<a href="https://www.google.com">Google</a><br>	
+	<a href="https://threatpost.com">ThreatPost</a><br>
+	<a href="https://dbu.onelogin.com/portal/">My DBU</a><br>
+	<a href="https://github.com/">My GitHub Repository</a><br>
+	
+	<p class="new">CSS Classes are in play here.  See my new class?</p>
+<p class="trial">Testing another class.</p>
+
+	<br>
+	<a href="anotherstyle.htm"> Check out the other pages for my stylesheet</a>
+<br>
+<br>
+
+<div>Questions?<a href="mailto:webmaster@somedomain.com"></a><a class="email"> Email Webmaster</a></div>
+
+<br>
+
+	<footer>
+	<small><i><b>Author:</b>Jennifer Thomas<br>
+	<b>Class:</b>STIM 3320<br>
+	<b>Date:</b>February 2, 2021</i></small>
+	</footer>
+
+	</body>
+</html>
